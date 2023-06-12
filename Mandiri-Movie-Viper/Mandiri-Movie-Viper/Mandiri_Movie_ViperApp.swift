f83f51e06479c99335b3f04826f42868bc9f15e6
Catharina Adinda Mega Cahyani <cadindamega@student.ciputra.ac.id>
2023-06-12 12:54:12 +0700

    create new project

---
+++ Mandiri-Movie-Viper/Mandiri-Movie-Viper/Mandiri_Movie_ViperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mandiri_Movie_ViperApp.swift
+//  Mandiri-Movie-Viper
+//
+//  Created by Catharina Adinda Mega Cahyani on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Mandiri_Movie_ViperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
